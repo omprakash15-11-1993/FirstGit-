cdf69a573d97dfd7dfa6ce4d41751065a066d5a2
admin <admin@192.168.2.8>
2021-04-11 00:32:01 +0530

    first commit

---
+++ GitDemo/src/com/abc/demo/Launch.java
@@ -0,0 +1,10 @@
+package com.abc.demo;
+
+public class Launch {
+
+	public static void main(String[] args) {
+    System.out.println("Welcome");
+    System.out.println("Welcome to git demo");
+	}
+
+}
